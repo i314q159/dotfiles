75df3e51f21fcd458d1c62f983784961d838b179
i314q159 <mchewenbao@outlook.com>
2021-02-19 09:27:17 +0800

    2021-02-19,09:02:17

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
+read -P "git commit -m:" Commit
+
 git add .
 
-cur_dateTime="`date +%Y-%m-%d,%H:%m:%S`"
-read -P "git commit -m:" Commit 
+cur_dateTime="`date +%Y-%m-%d,%H:%m:%S`" 
 git commit -m "$Commit $cur_dateTime"
 # XXX: Make git info less.
 
