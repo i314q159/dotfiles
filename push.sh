8a4cfd947238c2de9082874ab5639e10714c2833
i314q159 <mchewenbao@outlook.com>
2021-02-19 09:32:17 +0800

    adjust cur_dateTime format

--- push.sh
+++ push.sh
@@ -4,9 +4,9 @@ read -p "git commit -m:" Commit
 
 git add .
 
-cur_dateTime="`date +%Y-%m-%d,%H:%m:%S`" 
+cur_dateTime="`date +%Y-%m-%d %H:%m:%S`" 
 # read -p "git commit -m:" Commit
-git commit -m "$Commit $cur_dateTime"
+git commit -m "$Commit  $cur_dateTime"
 # XXX: Make git info less.
 
 git push
