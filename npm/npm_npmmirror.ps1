7351f0acd3a13f4d05620b33dade2e26828d7d8e
i314q159 <i314q159@skiff.com>
2022-06-05 22:10:31 +0800

    update node yarn

--- npm/npm_npmmirror.ps1
+++ npm/npm_npmmirror.ps1
@@ -1,1 +1,2 @@
-npm config set registry https://registry.npmmirror.com
+npm config --location=global set registry https://registry.npmmirror.com
+npm config --location=global get registry
--- yarn/yarn_npmmirror.ps1
+++ yarn/yarn_npmmirror.ps1
@@ -1,1 +1,2 @@
 yarn config set registry https://registry.npmmirror.com
+yarn config get registry
