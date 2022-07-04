fc981c94365940bd9d77ac6c6c9d4b7c6d759b41
i314q159 <i314q159@skiff.com>
2022-07-05 00:22:09 +0800

    update

---
+++ scoop/get-import.ps1
@@ -0,0 +1,9 @@
+function scoop_format($SCOOP_EXPORT) {
+        $SCOOP_INSTALL = "scoop install ";
+        Write-Output($SCOOP_INSTALL + $SCOOP_EXPORT.Split().GetValue(2).Trim("[]")+ "/" + $SCOOP_EXPORT.Split().GetValue(0)) >> import.ps1;
+}
+
+$FILE_PATH = "scoop.txt"
+foreach ($line in Get-Content $FILE_PATH) {
+        scoop_format($line);
+}
