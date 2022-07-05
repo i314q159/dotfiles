6cc0cb8bf38bc616bd07720fd471326bb056bc71
i314q159 <i314q159@skiff.com>
2022-07-05 08:35:37 +0800

    add Test-Path

--- scoop/get-import.ps1
+++ scoop/get-import.ps1
@@ -1,10 +1,21 @@
 function scoop_format($SCOOP_EXPORT) {
         $SCOOP_INSTALL = "scoop install ";
-        Write-Output($SCOOP_INSTALL + $SCOOP_EXPORT.Split().GetValue(2).Trim("[]")+ "/" + $SCOOP_EXPORT.Split().GetValue(0)) >> import.ps1;
+        Write-Output($SCOOP_INSTALL + $SCOOP_EXPORT.Split().GetValue(2).Trim("[]") + "/" + $SCOOP_EXPORT.Split().GetValue(0)) >> $IMPORT_FILE;
 }
 
-$FILE_PATH = "scoop.txt"
-Clear-Content import.ps1;
+$FILE_PATH = "scoop.txt";
+$IMPORT_FILE = "import.ps1";
+
+if (Test-Path $IMPORT_FILE) {
+        Write-Output($IMPORT_FILE);
+}
+else {
+        New-Item $IMPORT_FILE;
+
+}
+
+Clear-Content $IMPORT_FILE;
+
 foreach ($line in Get-Content $FILE_PATH) {
         scoop_format($line);
 }
