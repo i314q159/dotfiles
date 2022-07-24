d58d3d6e08b20ffbbcccbdb2bdb88a6e169f7629
i314q159 <i314q159@skiff.com>
2022-07-24 08:24:27 +0800

    scoop-complituon

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 # oh-my-posh
 oh-my-posh --init --shell pwsh --config ~/scoop/apps/oh-my-posh/current/themes/half-life.omp.json | Invoke-Expression
 
+# scoop-complituon
+Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
+
 # Alias
 Set-Alias l ls
 Set-Alias open explorer
