ef88440aa89a056444b033b051980d0474d30fb3
i314q159 <i314q159@skiff.com>
2022-08-10 12:48:12 +0800

    neovim

--- neovim/export.ps1
+++ neovim/export.ps1
@@ -1,2 +1,2 @@
-cp "~/AppData/Local/nvim/lua" -Recurse -Force "./nvim/lua";
+cp "~/AppData/Local/nvim/lua/*" -Recurse -Force "./nvim/lua";
 cp "~/AppData/Local/nvim/init.lua" -Recurse -Force "./nvim/init.lua";
--- neovim/nvim/lua/init-packer.lua
+++ neovim/nvim/lua/init-packer.lua
@@ -12,9 +12,4 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- Statusline
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
 end)
