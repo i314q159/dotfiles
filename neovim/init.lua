2a3a06cc98dfd4bdd2f6e5c9ac33d7677c6246b1
i314q159 <i314q159@skiff.com>
2022-08-10 10:31:56 +0800

    neovim

---
+++ neovim/init.lua
@@ -0,0 +1,12 @@
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.encoding = "utf-8"
+
+vim.o.termguicolors = true
+vim.o.background = "dark"
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
