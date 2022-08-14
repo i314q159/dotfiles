95a854be1b76e9b32c4dba004d0f1ac09226ad78
i314q159 <i314q159@skiff.com>
2022-08-14 09:49:24 +0800

    update

--- neovim/export.ps1
+++
@@ -1,2 +0,0 @@
-cp "~/AppData/Local/nvim/lua/*" -Recurse -Force "./nvim/lua";
-cp "~/AppData/Local/nvim/init.lua" -Recurse -Force "./nvim/init.lua";
--- neovim/nvim/lua/maps.lua
+++
@@ -1,6 +0,0 @@
--- Increment/Decrement
-vim.keymap.set('n', '+', '<C-a>')
-vim.keymap.set('n', '-', '<C-x>')
-
--- New tab
-vim.keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
