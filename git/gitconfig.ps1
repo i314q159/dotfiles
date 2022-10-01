ba975c0a0e04e08c3a915b2257314eed1c7f5f38
i314q159 <i314q159@outlook.com>
2022-10-02 02:06:04 +0800

    update

--- git/gitconfig.ps1
+++ git/gitconfig.ps1
@@ -1,6 +1,6 @@
 git config --global credential.helper store;
 git config --global user.name i314q159;
-git config --global user.email i314q159@skiff.com;
+git config --global user.email i314q159@outlook.com;
 
 git config --global user.name;
 git config --global user.email;
