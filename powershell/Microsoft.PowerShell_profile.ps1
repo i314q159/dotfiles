ee0b90a44bccce3d5e342166ffc4e0c75bfd596a
i314q159 <i314q159@outlook.com>
2022-10-23 20:58:07 +0800

    update

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+# scoop-completion
+Import-Module scoop-completion
+
+# oh-my-posh
+oh-my-posh init powershell --config='~/scoop/apps/oh-my-posh/current/themes/half-life.omp.json' | Invoke-Expression
+
+# alias
+Set-Alias open explorer
+Set-Alias l ls
+
