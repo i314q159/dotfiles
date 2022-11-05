5f67e5d234f58bbdb6fe400db32cf877d4336bb3
i314q159 <i314q159@outlook.com>
2022-11-05 15:47:47 +0800

    update

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,10 +1,12 @@
--- number of rows
+-- number
 vim.wo.number = true
 
 -- theme
 vim.cmd[[colorscheme gruvbox]]
 vim.o.background = "dark"
 vim.o.termguicolors = true
+vim.wo.cursorline = true
+vim.o.wildmenu = true
 
 -- utf8
 vim.g.encoding = "utf-8"
@@ -25,6 +27,25 @@ vim.o.swapfile = false
 
 -- mouse
 vim.o.mouse = false
+
+-- tab
+vim.o.tabstop = 4 
+vim.bo.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftround = true
+vim.o.shiftwidth = 4
+vim.bo.shiftwidth = 4
+vim.o.expandtab = true
+vim.bo.expandtab = true
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.smartindent = true
+
+-- search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = false
+vim.o.incsearch = false
 
 require('packer').startup(function()
     use 'wbthomason/packer.nvim'
