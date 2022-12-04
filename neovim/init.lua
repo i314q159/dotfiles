107e3cb043901aa2685e7c25d869bb961dfb7e97
i314q159 <i314q159@outlook.com>
2022-12-04 22:40:31 +0800

    update

--- neovim/init.lua
+++ neovim/init.lua
@@ -53,6 +53,14 @@ vim.o.completeopt = "menuone,noinsert,noselect"
 vim.o.loaded_python3_provider = 0
 vim.o.loaded_node_provider = 0
 
+-- keymap
+vim.g.mapleader = ';'
+vim.g.maplocalleader = ';'
+local map = vim.api.nvim_set_keymap
+local opt = { noremap = true, silent = true }
+map("n", "<C-u>", "9k", opt)
+map("n", "<C-d>", "9j", opt)
+
 require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     use 'terrortylor/nvim-comment'
@@ -61,6 +69,7 @@ require('packer').startup(function()
     use 'jghauser/mkdir.nvim' 
     use 'tanvirtin/monokai.nvim'
     use 'Pocco81/auto-save.nvim'
+    use 'lukas-reineke/indent-blankline.nvim'
 end)
 
 require('nvim_comment').setup({ line_mapping = '<C-]>' })
@@ -68,3 +77,4 @@ require('nvim-autopairs').setup()
 require('feline').setup()
 require('monokai').setup({ palette = require('monokai').pro })
 require('auto-save').setup()
+require('indent_blankline').setup()
