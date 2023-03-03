89ef2fa5a80fd7c9199453a3ddc9dd83597b55cd
i314q159 <i314q159@outlook.com>
2023-03-03 23:10:54 +0800

    cross platform

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,12 @@ local wezterm = require("wezterm")
 
 local M = {
 	window_background_opacity = 0.5,
-	default_prog = { "powershell", "-nologo" },
-	font = wezterm.font("Cascadia Code"),
 	font_size = 12.0,
 }
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	M.font = wezterm.font("Cascadia Code")
+	M.default_prog = { "powershell", "-nologo" }
+end
+
 return M
