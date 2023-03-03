96b41b3fcb8963ea1d2fcde8a19300067e23c837
i314q159 <i314q159@outlook.com>
2023-03-03 09:02:54 +0800

    update

--- clash/init.ps1
+++ clash/init.ps1
@@ -1,4 +1,4 @@
 # ~/.config/clash/config.yaml
 # mixed-port: 7890
 
-aria2c --allow-overwrite=true https://raw.fastgit.org/ripaojiedian/freenode/main/clash -o config.yaml
+aria2c --allow-overwrite=true https://ghproxy.com/https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash -o config.yaml
