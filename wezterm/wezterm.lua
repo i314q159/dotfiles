0e6ee0bd4187f157921afc3187114850b2fee69f
i314q159 <i314q159@outlook.com>
2023-03-08 21:15:14 +0800

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,8 @@
+-- x86_64-pc-windows-msvc - Windows
+-- x86_64-unknown-linux-gnu - Linux
+-- x86_64-apple-darwin - macOS (Intel)
+-- aarch64-apple-darwin - macOS (Apple Silicon)
+
 local wezterm = require("wezterm")
 
 local M = {
@@ -8,6 +13,8 @@ local M = {
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	M.font = wezterm.font("Cascadia Code")
 	M.default_prog = { "powershell", "-nologo" }
+elseif wezterm.target_triple == "x86_64-unknown-linux" then
+	M.default_prog = { "bash" }
 end
 
 return M
