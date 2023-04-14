3a513c54fde18eaefa9a69594cc64f0eb3c2d6f4
i314q159 <i314q159@outlook.com>
2023-04-14 08:29:31 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -157,4 +157,20 @@ require("lazy").setup({
 			require("dressing").setup()
 		end,
 	},
+	{
+		url = mirror .. "lewis6991/gitsigns.nvim",
+		enabled = vim.fn.executable("git") == 1,
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "▎" },
+					change = { text = "▎" },
+					delete = { text = "▎" },
+					topdelete = { text = "契" },
+					changedelete = { text = "▎" },
+					untracked = { text = "▎" },
+				},
+			})
+		end,
+	},
 })
