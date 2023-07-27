3539b9ec7d3e40327d3aaf2a0e2e5cceda2b30f3
i314q159 <i314q159@outlook.com>
2023-07-27 09:34:24 +0800

    update

--- scoop/bucket.ps1
+++ scoop/bucket.ps1
@@ -9,3 +9,6 @@ scoop bucket add java https://mirror.nju.edu.cn/git/scoop-java.git
 
 scoop bucket rm versions
 scoop bucket add versions https://mirror.nju.edu.cn/git/scoop-versions.git
+
+scoop bucket rm nonportable
+scoop bucket add nonportable https://mirror.nju.edu.cn/git/scoop-nonportable.git
