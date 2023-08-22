447abd1504d2438ef7c9161aae637cdad0554399
i314q159 <i314q159@outlook.com>
2023-08-22 14:04:36 +0800

    update

--- golang/init.ps1
+++ golang/init.ps1
@@ -1,12 +1,3 @@
 go env -w GO111MODULE=on
 
-# go env -w GOPROXY=""
-
-# https://goproxy.io/zh/
-go env -w GOPROXY="https://goproxy.io,direct"
-
-# https://goproxy.io/zh/
-# go env -w GOPROXY="https://proxy.golang.com.cn,direct"
-
-# https://goproxy.cn/
-# go env -w GOPROXY="https://goproxy.cn,direct"
+go env -w GOPROXY=""
