af304183124b71bf2afb870a73536f480c636745
i314q159 <i314q159@outlook.com>
2023-09-14 09:15:04 +0800

    update

--- clash/init.ps1
+++ clash/init.ps1
@@ -1,4 +1,2 @@
-# ~/.config/clash/config.yaml
-# mixed-port: 7890
-
-aria2c --allow-overwrite=true "https://ghproxy.com/https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash" -o config.yaml
+aria2c --allow-overwrite=true "https://ghproxy.com/https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash" -o ripaojiedian_freenode.yaml
+aria2c --allow-overwrite=true "https://ghproxy.com/https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list.yml" -o peasoft_NoMoreWalls.yml
