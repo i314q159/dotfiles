403218ad66d3bd6e868d7a4fc65ce170252742d8
i314q159 <i314q159@outlook.com>
2023-10-02 20:07:00 +0800

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -61,358 +61,370 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local mirror = "https://ghproxy.com/"
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        mirror .. "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		mirror .. "https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {
-    git = {
-        url_format = mirror .. "https://github.com/%s.git",
-    },
-    checker = {
-        enabled = true,
-        notify = false,
-    },
+	git = {
+		url_format = mirror .. "https://github.com/%s.git",
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
 }
 
 local plugins = {
-    {
-        "jghauser/mkdir.nvim",
-        "nvim-lua/plenary.nvim",
-    },
-    {
-        "axieax/urlview.nvim",
-        opts = {},
-    },
-    {
-        "nguyenvukhang/nvim-toggler",
-        opts = {},
-    },
-    {
-        "numToStr/Comment.nvim",
-        opts = {
-            toggler = {
-                line = "<C-]>",
-            },
-        },
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        config = function()
-            require("lualine").setup({
-                options = {
-                    icons_enabled = false,
-                    section_separators = "",
-                    component_separators = "",
-                },
-                sections = {
-                    lualine_x = {
-                        {
-                            require("lazy.status").updates,
-                            cond = require("lazy.status").has_updates,
-                        },
-                        {
-                            require("ac").get_active_lsp_clients,
-                            icon = "",
-                        },
-                        {
-                            "datetime",
-                            style = "iso",
-                        },
-                        {
-                            "%#lualine_fg#tab:%{&tabstop}%#lualine_inactive#",
-                        },
-                        {
-                            "%#lualine_fg#%{&fileencoding}%#lualine_inactive#",
-                        },
-                        {
-                            "filetype",
-                        },
-                    },
+	{
+		"jghauser/mkdir.nvim",
+		"nvim-lua/plenary.nvim",
+	},
+	{
+		"axieax/urlview.nvim",
+		opts = {},
+	},
+	{
+		"nguyenvukhang/nvim-toggler",
+		opts = {},
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			toggler = {
+				line = "<C-]>",
+			},
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					section_separators = "",
+					component_separators = "",
+				},
+				sections = {
+					lualine_x = {
+						{
+							require("lazy.status").updates,
+							cond = require("lazy.status").has_updates,
+						},
+						{
+							require("ac").get_active_lsp_clients,
+							icon = "",
+						},
+						{
+							"datetime",
+							style = "iso",
+						},
+						{
+							"%#lualine_fg#tab:%{&tabstop}%#lualine_inactive#",
+						},
+						{
+							"%#lualine_fg#%{&fileencoding}%#lualine_inactive#",
+						},
+						{
+							"filetype",
+						},
+					},
 
-                    lualine_c = {
-                        {
-                            "filename",
-                            path = 2,
-                        },
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "windwp/nvim-autopairs",
-        opts = {},
-    },
-    {
-        "toppair/reach.nvim",
-        opts = {},
-        keys = {
-            { "<leader>b", "<cmd>ReachOpen buffers<cr>",      desc = "ReachOpen buffers" },
-            { "<leader>t", "<cmd>ReachOpen colorschemes<cr>", desc = "ReachOpen colorschemes" },
-        },
-    },
-    {
-        "ethanholz/nvim-lastplace",
-        opts = {},
-    },
-    {
-        "NvChad/nvim-colorizer.lua",
-        opts = {
-            user_default_options = {
-                mode = "virtualtext",
-            },
-        },
-    },
-    {
-        "chentoast/marks.nvim",
-        opts = {},
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = {},
-        keys = {
-            { "<leader>h", "<cmd>Gitsigns preview_hunk<cr>", desc = "Gitsigns preview_hunk" },
-            { "<leader>n", "<cmd>Gitsigns next_hunk<cr>",    desc = "Gitsigns next_hunkk" },
-        },
-        lazy = false,
-    },
-    {
-        "folke/which-key.nvim",
-        opts = {
-            auto_show = false,
-        },
-        keys = {
-            { "<leader>k", "<cmd>WhichKey<cr>", desc = "WhichKey" },
-        },
-        lazy = false,
-    },
-    {
-        "junnplus/lsp-setup.nvim",
-        dependencies = {
-            "neovim/nvim-lspconfig",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-        },
-        config = function()
-            require("lsp-setup").setup({
-                servers = {
-                    bashls = {},
-                    denols = {},
-                    gopls = {},
-                    html = {},
-                    pylsp = {},
-                    pyright = {},
+					lualine_c = {
+						{
+							"filename",
+							path = 2,
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		opts = {},
+	},
+	{
+		"toppair/reach.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>b", "<cmd>ReachOpen buffers<cr>", desc = "ReachOpen buffers" },
+			{ "<leader>t", "<cmd>ReachOpen colorschemes<cr>", desc = "ReachOpen colorschemes" },
+		},
+	},
+	{
+		"ethanholz/nvim-lastplace",
+		opts = {},
+	},
+	{
+		"NvChad/nvim-colorizer.lua",
+		opts = {
+			user_default_options = {
+				mode = "virtualtext",
+			},
+		},
+	},
+	{
+		"chentoast/marks.nvim",
+		opts = {},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>h", "<cmd>Gitsigns preview_hunk<cr>", desc = "Gitsigns preview_hunk" },
+			{ "<leader>n", "<cmd>Gitsigns next_hunk<cr>", desc = "Gitsigns next_hunkk" },
+		},
+		lazy = false,
+	},
+	{
+		"folke/which-key.nvim",
+		opts = {
+			auto_show = false,
+		},
+		keys = {
+			{ "<leader>k", "<cmd>WhichKey<cr>", desc = "WhichKey" },
+		},
+		lazy = false,
+	},
+	{
+		"junnplus/lsp-setup.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("lsp-setup").setup({
+				servers = {
+					bashls = {},
+					denols = {},
+					gopls = {},
+					html = {},
+					pylsp = {},
+					pyright = {},
 
-                    lua_ls = {
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" },
-                                },
-                                workspace = {
-                                    library = vim.api.nvim_get_runtime_file("", true),
-                                    checkThirdParty = false,
-                                },
-                                telemetry = {
-                                    enable = false,
-                                },
-                                format = {
-                                    enable = true,
-                                },
-                            },
-                        },
-                    },
-                },
-            })
+					lua_ls = {
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = vim.api.nvim_get_runtime_file("", true),
+									checkThirdParty = false,
+								},
+								telemetry = {
+									enable = false,
+								},
+								format = {
+									enable = false,
+								},
+							},
+						},
+					},
+				},
+			})
 
-            require("mason").setup({
-                github = {
-                    download_url_template = mirror .. "https://github.com/%s/releases/download/%s/%s",
-                },
-                ui = {
-                    icons = {
-                        package_installed = "++",
-                        package_pending = "->",
-                        package_uninstalled = "--",
-                    },
-                },
-            })
+			require("mason").setup({
+				github = {
+					download_url_template = mirror .. "https://github.com/%s/releases/download/%s/%s",
+				},
+				ui = {
+					icons = {
+						package_installed = "++",
+						package_pending = "->",
+						package_uninstalled = "--",
+					},
+				},
+			})
 
-            require("mason-lspconfig").setup()
-        end,
-    },
-    {
-        "folke/neodev.nvim",
-        opts = {},
-    },
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-        },
-        config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-calc",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip",
-        },
-        config = function()
-            local cmp = require("cmp")
+			require("mason-lspconfig").setup()
+		end,
+	},
+	{
+		"folke/neodev.nvim",
+		opts = {},
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-calc",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			local cmp = require("cmp")
 
-            cmp.setup({
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "path" },
-                    { name = "buffer" },
-                    { name = "calc" },
-                    { name = "crates" },
-                }),
-                mapping = {
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        select = false,
-                        behavior = cmp.ConfirmBehavior.Replace,
-                    }),
-                },
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-            })
-        end,
-    },
-    {
-        "saecki/crates.nvim",
-        opts = {},
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.install").prefer_git = true
-            for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
-                config.install_info.url =
-                    config.install_info.url:gsub("https://github.com/", mirror .. "https://github.com/")
-            end
+			cmp.setup({
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+					{ name = "buffer" },
+					{ name = "calc" },
+					{ name = "crates" },
+				}),
+				mapping = {
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+					["<CR>"] = cmp.mapping.confirm({
+						select = false,
+						behavior = cmp.ConfirmBehavior.Replace,
+					}),
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"saecki/crates.nvim",
+		opts = {},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.install").prefer_git = true
+			for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
+				config.install_info.url =
+					config.install_info.url:gsub("https://github.com/", mirror .. "https://github.com/")
+			end
 
-            require("nvim-treesitter.configs").setup({
-                highlight = {
-                    enable = true,
-                },
-                auto_install = true,
-                ensure_installed = {
-                    "go",
-                    "json",
-                    "lua",
-                    "toml",
-                    "vim",
-                    "vimdoc",
-                    "python",
-                },
-                indent = {
-                    enable = true,
-                },
-            })
-        end,
-    },
-    {
-        "nvimdev/hlsearch.nvim",
-        opts = {},
-    },
-    {
-        "Wansmer/treesj",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-        },
-        keys = {
-            { "<leader>j", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
-        },
-        opts = {
-            use_default_keymaps = false,
-            max_join_length = 150 * 2,
-        },
-    },
-    {
-        "nacro90/numb.nvim",
-        opts = {},
-    },
-    {
-        "folke/todo-comments.nvim",
-        lazy = false,
-        opts = {},
-    },
-    {
-        "VidocqH/lsp-lens.nvim",
-        opts = {},
-    },
-    {
-        "jinzhongjia/LspUI.nvim",
-        opts = {},
-        keys = {
-            { "<F2>", "<cmd>LspUI rename<cr>", desc = "LspUI rename" },
-        },
-    },
-    {
-        "stevearc/dressing.nvim",
-        opts = {},
-    },
-    {
-        "elentok/togglr.nvim",
-        opts = {
-            key = "<leader>i",
-        },
-    },
-    {
-        "elentok/scriptify.nvim",
-        opts = {},
-        cmd = { "Scriptify" },
-    },
-    {
-        "i314q159/ac.nvim",
-        opts = {},
-    },
-    {
-        "Mofiqul/dracula.nvim",
-        opts = {
-            transparent_bg = false,
-            italic_comment = true,
-        },
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        "nvimdev/indentmini.nvim",
-        opts = {
-            char = "┊",
-            exclude = {
-                "markdown",
-            },
-        },
-    },
-    {
-        "roobert/f-string-toggle.nvim",
-        opts = {
-            key_binding = "<leader>f"
-        },
-    },
+			require("nvim-treesitter.configs").setup({
+				highlight = {
+					enable = true,
+				},
+				auto_install = true,
+				ensure_installed = {
+					"go",
+					"json",
+					"lua",
+					"toml",
+					"vim",
+					"vimdoc",
+					"python",
+				},
+				sync_install = true,
+				indent = {
+					enable = true,
+				},
+			})
+		end,
+	},
+	{
+		"nvimdev/hlsearch.nvim",
+		opts = {},
+	},
+	{
+		"Wansmer/treesj",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		keys = {
+			{ "<leader>j", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+		},
+		opts = {
+			use_default_keymaps = false,
+			max_join_length = 150 * 2,
+		},
+	},
+	{
+		"nacro90/numb.nvim",
+		opts = {},
+	},
+	{
+		"folke/todo-comments.nvim",
+		lazy = false,
+		opts = {},
+	},
+	{
+		"VidocqH/lsp-lens.nvim",
+		opts = {},
+	},
+	{
+		"jinzhongjia/LspUI.nvim",
+		opts = {},
+		keys = {
+			{ "<F2>", "<cmd>LspUI rename<cr>", desc = "LspUI rename" },
+		},
+	},
+	{
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+	{
+		"elentok/togglr.nvim",
+		opts = {
+			key = "<leader>i",
+		},
+	},
+	{
+		"elentok/scriptify.nvim",
+		opts = {},
+		cmd = { "Scriptify" },
+	},
+	{
+		"i314q159/ac.nvim",
+		opts = {},
+	},
+	{
+		"Mofiqul/dracula.nvim",
+		opts = {
+			transparent_bg = false,
+			italic_comment = true,
+		},
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"roobert/f-string-toggle.nvim",
+		opts = {
+			key_binding = "<leader>f",
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			format_on_save = {
+				timeout_ms = 100,
+				lsp_fallback = false,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = {
+					"isort",
+					"black",
+				},
+			},
+		},
+	},
+	{
+		"pocco81/auto-save.nvim",
+		opts = {},
+	},
 }
 
 require("lazy").setup(plugins, opts)
