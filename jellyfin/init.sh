5162ecdd8ff0c58c7240467ec1ae4decf5759cf5
i314q159 <i314q159@outlook.com>
2023-10-19 15:20:42 +0800

    update

--- jellyfin/init.sh
+++ jellyfin/init.sh
@@ -1,2 +1,3 @@
-mkdir -pv ~/podman/jellyfin/{config,data}
-podman run -d --name=jellyfin -e TZ=Asia/Shanghai -p 8096:8096 -p 8920:8920 -p 7359:7359/udp -p 1900:1900/udp -v ~/podman/jellyfin/config:/config -v ~/podman/jellyfin/data:/data --restart unless-stopped lscr.io/linuxserver/jellyfin:latest
+mkdir -pv ~/podman/jellyfin/config
+mkdir -pv ~/podman/qbittorrent/downloads
+podman run -d --name=jellyfin -e TZ=Asia/Shanghai -p 8096:8096 -p 8920:8920 -p 7359:7359/udp -p 1900:1900/udp -v ~/podman/jellyfin/config:/config -v ~/podman/qbittorrent/downloads:/data --restart unless-stopped lscr.io/linuxserver/jellyfin:latest
