e9871ce63145f01534c54c532a6422a50b77efcf
i314q159 <i314q159@outlook.com>
2023-10-19 15:16:38 +0800

    update

--- jellyfin/init.sh
+++ jellyfin/init.sh
@@ -1,14 +1,2 @@
 mkdir -pv ~/podman/jellyfin/{config,data}
-podman run -d \
-  --name=jellyfin \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Asia/Shanghai \
-  -p 8096:8096 \
-  -p 8920:8920 `#optional` \
-  -p 7359:7359/udp `#optional` \
-  -p 1900:1900/udp `#optional` \
-  -v ~/podman/jellyfin/config:/config \
-  -v ~/podman/jellyfin/data:/data \
-  --restart unless-stopped \
-  lscr.io/linuxserver/jellyfin:latest
+podman run -d --name=jellyfin -e TZ=Asia/Shanghai -p 8096:8096 -p 8920:8920 -p 7359:7359/udp -p 1900:1900/udp -v ~/podman/jellyfin/config:/config -v ~/podman/jellyfin/data:/data --restart unless-stopped lscr.io/linuxserver/jellyfin:latest
--- qbittorrent/init.sh
+++ qbittorrent/init.sh
@@ -1,3 +1,3 @@
 # username/password is admin/adminadmin.
 mkdir -pv ~/podman/qbittorrent/{config,downloads}
-podman run -d --name=qbittorrent -e PUID=1000 -e PGID=1000 -e TZ=Asia/Shanghai -e WEBUI_PORT=2333 -p 2333:2333 -p 6881:6881 -p 6881:6881/udp -v ~/podman/qbittorrent/config:/config -v ~/podman/qbittorrent/downloads:/downloads --restart unless-stopped lscr.io/linuxserver/qbittorrent:latest
+podman run -d --name=qbittorrent -e TZ=Asia/Shanghai -e WEBUI_PORT=2333 -p 2333:2333 -p 6881:6881 -p 6881:6881/udp -v ~/podman/qbittorrent/config:/config -v ~/podman/qbittorrent/downloads:/downloads --restart unless-stopped lscr.io/linuxserver/qbittorrent:latest
