6dc60194196b8a83d609729445010d15de8a82e6
i314q159 <i314q159@outlook.com>
2023-10-25 20:06:43 +0800

    update

--- wsl/alpine.ps1
+++
@@ -1,5 +0,0 @@
-podman run --name=alpine docker.io/library/alpine
-podman export alpine > d:/wsl_d/alpine/alpine.tar
-podman stop alpine
-podman rm alpine
-wsl --import alpine d:/wsl_d/alpine d:/wsl_d/alpine/alpine.tar --version 2
