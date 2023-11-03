1de802b723cad1ab6b7b2d50803885ff51dba9b8
i314q159 <i314q159@outlook.com>
2023-11-03 15:06:49 +0800

    update

--- clash/profile.ps1
+++ clash/profile.ps1
@@ -1,2 +1,2 @@
-aria2c --allow-overwrite=true "https://ghproxy.com/https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash" -o ripaojiedian_freenode.yaml
-aria2c --allow-overwrite=true "https://ghproxy.com/https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list.yml" -o peasoft_NoMoreWalls.yml
+aria2c --allow-overwrite=true "https://ghproxy.net/https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash" -o ripaojiedian_freenode.yaml
+aria2c --allow-overwrite=true "https://ghproxy.net/https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list.yml" -o peasoft_NoMoreWalls.yml
