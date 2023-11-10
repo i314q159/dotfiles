993d230f4b704d14389d2f9f0aa9a6bb1cd20c17
i314q159 <i314q159@outlook.com>
2023-11-10 11:37:21 +0800

    update

--- rustup/init.ps1
+++ rustup/init.ps1
@@ -1,2 +1,6 @@
 setx RUSTUP_UPDATE_ROOT https://mirrors.cernet.edu.cn/rustup/rustup
 setx RUSTUP_DIST_SERVER https://mirrors.cernet.edu.cn/rustup
+
+rustup toolchain install stable-gnu
+rustup default stable-gnu
+rustup toolchain uninstall stable-msvc
