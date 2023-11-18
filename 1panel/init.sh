a50ddad9bc78018a382481b30a9eb0b0c60d8e20
i314q159 <i314q159@outlook.com>
2023-11-18 10:10:17 +0800

    update

---
+++ 1panel/init.sh
@@ -0,0 +1,5 @@
+# root
+curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o quick_start.sh
+sh quick_start.sh
+
+1pctl user-info
--- docker/init.sh
+++ docker/init.sh
@@ -1,2 +1,9 @@
+# root
+curl -fsSL https://get.docker.com -o install-docker.sh
+sh install-docker.sh --mirror Aliyun
+systemctl enable docker
+usermod -aG docker i314q159
+
+# current user
 sudo usermod -aG docker $USER
 sudo reboot
