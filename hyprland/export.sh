a983ee5d6cab85a192d102727d689d61fa2e77aa
i314q159 <i314q159@outlook.com>
2023-11-25 15:50:06 +0800

    update

--- hyprland/export.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-cp ~/.config/hypr/hyprland.conf ./hypr/hyprland.conf
-cp ~/.config/kitty/kitty.conf ./kitty/kitty.conf
---
+++ kitty/export.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cp ~/.config/kitty/kitty.conf ./kitty.conf
