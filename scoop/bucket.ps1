0b06c916a1777832b15fcfc43b1107cca5e3ca74
i314q159 <i314q159@outlook.com>
2023-12-08 14:49:59 +0800

    update

--- scoop/bucket.ps1
+++ scoop/bucket.ps1
@@ -1,4 +1,4 @@
-scoop bucket add main https://mirror.nju.edu.cn/git/scoop-main.git
-scoop bucket add extras https://mirror.nju.edu.cn/git/scoop-extras.git
-scoop bucket add java https://mirror.nju.edu.cn/git/scoop-java.git
-scoop bucket add versions https://mirror.nju.edu.cn/git/scoop-versions.git
+scoop bucket add main
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add versions
