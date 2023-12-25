278af0d95a51842b19915a70ce5674257b62831e
i314q159 <i314q159@outlook.com>
2023-12-25 14:25:08 +0800

    update

--- docker/init.sh
+++ docker/init.sh
@@ -4,6 +4,5 @@ sh install-docker.sh --mirror Aliyun
 systemctl enable docker
 usermod -aG docker i314q159
 
-# current user
-sudo usermod -aG docker $USER
-sudo reboot
+systemctl daemon-reload
+systemctl restart docker
