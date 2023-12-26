8e8a0ff6206e5cd53fefedc2c672d8c80fac0f49
i314q159 <i314q159@outlook.com>
2023-12-26 09:43:41 +0800

    update

--- docker/init.sh
+++ docker/init.sh
@@ -1,8 +1,6 @@
 # root
-curl -fsSL https://get.docker.com -o install-docker.sh
-sh install-docker.sh --mirror Aliyun
+curl -fsSL https://get.docker.com -o install-docker.sh && sh install-docker.sh --mirror Aliyun
 systemctl enable docker
 usermod -aG docker i314q159
-
 systemctl daemon-reload
 systemctl restart docker
