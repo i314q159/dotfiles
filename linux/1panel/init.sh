6ae1be177e08ca4b8bafc4acfe321479e94dbca9
i314q159 <i314q159@outlook.com>
2024-01-18 14:30:26 +0800

    fit2cloud

--- linux/1panel/init.sh
+++ linux/1panel/init.sh
@@ -1,3 +1,4 @@
 # root
-curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o quick_start.sh && bash quick_start.sh
+curl -sSL https://resource.fit2cloud.com/1panel/package/quick_start.sh -o 1panel_quick_start.sh
+bash 1panel_quick_start.sh
 1pctl user-info
