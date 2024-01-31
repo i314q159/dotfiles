3fb4fbb1cfc0e23deae40280623085bc8d209579
i314q159 <i314q159@outlook.com>
2024-01-31 16:32:26 +0800

    update

--- linux/nvim/init.sh
+++ linux/nvim/init.sh
@@ -1,5 +1,6 @@
 curl -O https://ghproxy.net/https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 tar -xzvf nvim-linux64.tar.gz -C ~/bin
+rm nvim-linux64.tar.gz
 
 export PATH=$HOME/bin:$PATH
 export PATH=$HOME/bin/nvim-linux64/bin:$PATH
