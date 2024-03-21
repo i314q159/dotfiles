7a06297bd05b09a580b702a1999397891553094d
i314q159 <i314q159@outlook.com>
2024-03-21 08:55:44 +0800

    update

--- docker/registry/init.sh
+++ docker/registry/init.sh
@@ -1,2 +1,2 @@
-mkdir -pv ~/docker/registry
-docker run -d -p 5000:5000 -v ~/docker/registry:/var/lib/registry --name=registry docker.io/library/registry
+mkdir -pv ~/registry
+docker run -d -p 5000:5000 -v ~/registry:/var/lib/registry --name=registry docker.io/library/registry
