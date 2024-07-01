326fcdff10c58b96102b62680b8f940a5c2a9227
i314q159 <i314q159@outlook.com>
2024-07-01 16:25:18 +0800

    Big brother is watching you

--- docker/portainer/init.sh
+++ docker/portainer/init.sh
@@ -1,1 +1,1 @@
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock --name portaine docker.io/portainer/portainer-ce
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v ~/portainer/data:/data --name portainer docker.io/portainer/portainer-ce
