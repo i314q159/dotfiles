d00ed3d5efd865135abf42ff3c7e3a40e42c35fc
i314q159 <i314q159@outlook.com>
2024-09-04 19:31:24 +0800

    Update init.lua

--- windows/nvim/init.lua
+++ windows/nvim/init.lua
@@ -116,18 +116,6 @@ local lsps = {
 		},
 	},
 	{
-		"stevearc/conform.nvim",
-		opts = {
-			format_on_save = {
-				timeout_ms = 500,
-				lsp_fallback = false,
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-			},
-		},
-	},
-	{
 		"junnplus/lsp-setup.nvim",
 		opts = {
 			servers = {
