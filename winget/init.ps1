57e2b32b7a6703f3a8127c481ad8364635fae67d
i314q159 <i314q159@outlook.com>
2025-08-02 18:31:46 +0800

    Big brother is watching you

--- winget/init.ps1
+++ winget/init.ps1
@@ -1,3 +1,2 @@
-# winget source add cernet https://mirrors.cernet.edu.cn/winget-source
 winget settings set DefaultProxy http://127.0.0.1:12334
-# winget settings reset DefaultProxy
+winget settings reset DefaultProxy
